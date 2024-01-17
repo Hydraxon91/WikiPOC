7c3a137d70871e9a8d3b6e174ff9d32dfb44c8be
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-01-17 18:30:51 +0100

    Created models for wikipage and paragraphs

---
+++ wiki-backend/wiki-backend/Models/Paragraph.cs
@@ -0,0 +1,9 @@
+﻿namespace wiki_backend.Models;
+
+public class Paragraph
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public string ParagraphImage { get; set; }
+    public string ParagraphImageText { get; set; }
+}
---
+++ wiki-backend/wiki-backend/Models/WikiPage.cs
@@ -0,0 +1,10 @@
+﻿namespace wiki_backend.Models;
+
+public class WikiPage
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string SiteSub { get; set; }
+    public string RoleNote { get; set; }
+    public List<Paragraph> Paragraphs { get; set; }
+}
