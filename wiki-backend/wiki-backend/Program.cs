6cb1a1caad85614c56f9053ab73672d6540ae1f8
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-01-18 13:24:20 +0100

    Added AddCors and AddDbContext to program.cs

--- wiki-backend/wiki-backend/Program.cs
+++ wiki-backend/wiki-backend/Program.cs
@@ -1,6 +1,10 @@
+using wiki_backend.DatabaseServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+AddCors();
+AddDbContext();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -9,6 +13,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseCors();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -23,3 +28,28 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+void AddCors()
+{
+    builder.Services.AddCors(options =>
+    {
+        options.AddDefaultPolicy(
+            policy =>
+            {
+                policy.WithOrigins("*")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+            });
+        options.AddPolicy("AllowLocalhost", policy =>
+        {
+            policy
+                .WithOrigins("http://localhost:3000", "http://localhost:3001") // Allow requests from your client's origin
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+    });
+}
+void AddDbContext()
+{
+    builder.Services.AddDbContext<WikiDbContext>();
+}
