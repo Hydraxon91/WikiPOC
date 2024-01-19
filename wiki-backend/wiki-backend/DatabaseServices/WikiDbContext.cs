06420e92c323dc9d7a3b94f299c3b6472ab13837
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-01-19 16:51:17 +0100

    Added connection string to dbcontext

--- wiki-backend/wiki-backend/DatabaseServices/WikiDbContext.cs
+++ wiki-backend/wiki-backend/DatabaseServices/WikiDbContext.cs
@@ -1,17 +1,31 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using wiki_backend.Models;
-using wiki_backend.Objects;
 
 namespace wiki_backend.DatabaseServices;
 
-public class WikiDbContext : DbContext
+public class WikiDbContext : IdentityDbContext
 {
-    public DbSet<User> Users { get; set; }
+    private readonly IConfiguration _configuration;
+
+    public WikiDbContext(DbContextOptions<WikiDbContext> options, IConfiguration configuration)
+        : base(options)
+    {
+        _configuration = configuration;
+    }
     public DbSet<WikiPage> WikiPages { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         // Add any additional model configuration here
         // For example, configuring relationships, setting primary keys, etc.
+    }
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Add DbContext with IConfiguration dependency
+        services.AddDbContext<WikiDbContext>(options =>
+            options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
+        
     }
 }
