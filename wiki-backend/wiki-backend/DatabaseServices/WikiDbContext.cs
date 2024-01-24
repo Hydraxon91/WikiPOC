9ad2524994f8776b698458f3f2301464fcd10bc3
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-01-24 10:21:11 +0100

    Adds seeding

--- wiki-backend/wiki-backend/DatabaseServices/WikiDbContext.cs
+++ wiki-backend/wiki-backend/DatabaseServices/WikiDbContext.cs
@@ -21,10 +21,63 @@ public class WikiDbContext : IdentityDbContext
     {
         base.OnModelCreating(modelBuilder);
 
-        // modelBuilder.Entity<WikiPage>()
-        //     .HasMany<Paragraph>()
-        //     .WithOne(p => p.WikiPage)
-        //     .HasForeignKey(p => p.WikiPageId);
+        modelBuilder.Entity<WikiPage>().HasMany(wp => wp.Paragraphs)
+            .WithOne(p => p.WikiPage)
+            .HasForeignKey(p => p.WikiPageId)
+            .HasPrincipalKey(p => p.Id);
+        
+        modelBuilder.Entity<WikiPage>().HasData(
+            new WikiPage
+            {
+                Id = 1,
+                Title = "Example Page 1",
+                SiteSub = "Example SiteSub 1",
+                RoleNote = "Example RoleNote 1",
+            },
+            new WikiPage
+            {
+                Id = 2,
+                Title = "Example Page 2",
+                SiteSub = "Example SiteSub 2",
+                RoleNote = "Example RoleNote 2",
+            }
+        );
+        
+        modelBuilder.Entity<Paragraph>().HasData(
+            new Paragraph
+            {
+                Id = 1,
+                WikiPageId = 1,
+                Title = "Example Paragraph 1",
+                Content = "Example content 1",
+                ParagraphImage = "https://html5-templates.com/demo/wikipedia-template/img/pencil.jpg",
+                ParagraphImageText = "Example ParagraphImageText 1"
+            },
+            new Paragraph
+            {
+                Id = 2,
+                WikiPageId = 1,
+                Title = "Example Paragraph 2",
+                Content = "Example content 2"
+            },
+            new Paragraph
+            {
+                Id = 3,
+                WikiPageId = 2,
+                Title = "Example Paragraph 3",
+                Content = "Example content 3",
+                ParagraphImage = "https://html5-templates.com/demo/wikipedia-template/img/pencil.jpg",
+                ParagraphImageText = "Example ParagraphImageText3"
+            },
+            new Paragraph
+            {
+                Id = 4,
+                WikiPageId = 2,
+                Title = "Example Paragraph 4",
+                Content = "Example content 4"
+            }
+            );
+        
         // Add any additional model configuration here
         // For example, configuring relationships, setting primary keys, etc.
     }
@@ -35,4 +88,5 @@ public class WikiDbContext : IdentityDbContext
         Console.WriteLine(connectionString);
         optionsBuilder.UseSqlServer(connectionString);
     }
+    
 }
--- wiki-backend/wiki-backend/Migrations/20240124091402_InitialMigration2.Designer.cs
+++ wiki-backend/wiki-backend/Migrations/20240124091402_InitialMigration2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using wiki_backend.DatabaseServices;
 
@@ -11,9 +12,11 @@ using wiki_backend.DatabaseServices;
 namespace wiki_backend.Migrations
 {
     [DbContext(typeof(WikiDbContext))]
-    partial class WikiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240124091402_InitialMigration2")]
+    partial class InitialMigration2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
