0e32c3757535273e37f187aae6bfda4e315712c9
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-02-06 10:55:03 +0100

    Adds IAuthService interface

---
+++ wiki-backend/wiki-backend/Services/Authentication/IAuthService.cs
@@ -0,0 +1,7 @@
+﻿namespace wiki_backend.Services.Authentication;
+
+public interface IAuthService
+{
+    Task<AuthResult> RegisterAsync(string email, string username, string password, string role);
+    Task<AuthResult> LoginAsync(string username, string password);
+}
