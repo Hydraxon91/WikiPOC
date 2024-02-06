5e564f871ea7245289ae8ecac9fba21bbccd226a
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-02-06 10:31:04 +0100

    Sets up Jwt authentication

--- wiki-backend/wiki-backend/Program.cs
+++ wiki-backend/wiki-backend/Program.cs
@@ -1,7 +1,36 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using wiki_backend.DatabaseServices;
 using wiki_backend.DatabaseServices.Repositories;
+using wiki_backend.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+       ValidIssuer = config["JwtSettings:Issuer"],
+       ValidAudience = config["JwtSettings:Audience"],
+       IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JwtSettings:Key"]!)),
+       ValidateIssuer = true,
+       ValidateAudience = true,
+       ValidateLifetime = true,
+       ValidateIssuerSigningKey = true
+    };
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy(IdentityData.AdminUserPolicyName, p => p.RequireClaim(IdentityData.AdminUserClaimName, "true"));
+});
 
 // Add services to the container.
 AddCors();
@@ -28,6 +57,7 @@ app.UseHttpsRedirection();
 app.UseCors("AllowLocalhost");
 app.UseCors();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
