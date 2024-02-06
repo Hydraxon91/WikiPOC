e0ae3ae3e63b4aa87ab19a1074ebfe26157b0cce
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-02-06 10:52:43 +0100

    Adds ITokenServices interface

---
+++ wiki-backend/wiki-backend/Services/Authentication/ITokenServices.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace wiki_backend.Services.Authentication;
+
+public interface ITokenServices
+{
+    public string CreateToken(IdentityUser user, string role); 
+}
