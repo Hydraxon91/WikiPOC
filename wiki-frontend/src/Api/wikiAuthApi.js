bca210b5643c78e0b4d4f59c8bf54e5f23f18dcb
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-02-07 18:37:47 +0100

    Adds registration fetch to wikiauthapi.js

--- wiki-frontend/src/Api/wikiAuthApi.js
+++ wiki-frontend/src/Api/wikiAuthApi.js
@@ -15,3 +15,21 @@ export const handleLoginSubmit = async (email, password) => {
     const responseData = await response.json();
     return responseData;
 };
+
+export const handleRegisterSubmit = async (email, username, password, role) => {
+    const data = {
+        email: email,
+        username: username,
+        password: password,
+        role: role
+    }
+    const response = await fetch(`${BASE_URL}/api/Auth/Register`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+        });
+    const responseData = await response.json();
+    return responseData;
+};
