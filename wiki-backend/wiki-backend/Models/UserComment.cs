f44661f54a3f886977e193b124e905240af692df
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-02-26 17:03:18 +0100

    Creates UserProfile.cs

---
+++ wiki-backend/wiki-backend/Models/UserComment.cs
@@ -0,0 +1,11 @@
+﻿namespace wiki_backend.Models;
+
+public class UserComment
+{
+    public int Id { get; set; }
+    public string? Content { get; set; }
+    public UserProfile? UserProfile { get; set; }
+    public DateTime PostDate { get; set; }
+    public bool IsReply { get; set; }
+    public UserComment? ReplyToComment { get; set; }
+}
