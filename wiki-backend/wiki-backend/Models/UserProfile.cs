95dc47275afbc3a7ee86069eabfc6a0dff7625b4
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-02-26 17:02:29 +0100

    Creates UserProfile.cs and ApplicationUser.cs

---
+++ wiki-backend/wiki-backend/Models/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace wiki_backend.Models;
+
+public class UserProfile
+{
+    public int Id { get; set; }
+    public string? UserId { get; set; } 
+    public string? UserName { get; set; }
+    public string? ProfilePicture { get; set; }
+    public string? DisplayName { get; set; }
+    [JsonIgnore]
+    public ApplicationUser? User { get; set; }
+}
