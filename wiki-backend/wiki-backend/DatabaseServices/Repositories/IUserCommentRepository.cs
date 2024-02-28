67522966d801e0b845d58f4ceae46c39b954c2d4
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-02-28 09:57:55 +0100

    Creates UserCommentRepository

---
+++ wiki-backend/wiki-backend/DatabaseServices/Repositories/IUserCommentRepository.cs
@@ -0,0 +1,11 @@
+﻿using wiki_backend.Models;
+
+namespace wiki_backend.DatabaseServices.Repositories;
+
+public interface IUserCommentRepository
+{
+    Task<UserComment?> GetByIdAsync(int id);
+    Task AddAsync(UserComment comment);
+    Task UpdateAsync(int commentId, string updatedContent);
+    Task DeleteAsync(int id);
+}
---
+++ wiki-backend/wiki-backend/DatabaseServices/Repositories/UserCommentRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using wiki_backend.Models;
+
+namespace wiki_backend.DatabaseServices.Repositories;
+
+public class UserCommentRepository : IUserCommentRepository
+{
+    private readonly WikiDbContext _context;
+
+    public UserCommentRepository(WikiDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<UserComment?> GetByIdAsync(int id)
+    {
+        return await _context.UserComments
+            .SingleOrDefaultAsync(uc => uc.Id == id);
+    }
+
+    public async Task AddAsync(UserComment comment)
+    {
+        _context.UserComments.Add(comment);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(int commentId, string updatedContent)
+    {
+        var existingComment = await _context.UserComments.SingleOrDefaultAsync(uc => uc.Id == commentId);
+        if (existingComment == null)
+        {
+            throw new InvalidOperationException($"Comment with ID {commentId} not found.");
+        }
+
+        existingComment.Content = updatedContent;
+        existingComment.IsEdited = true;
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var existingComment = await _context.UserComments.SingleOrDefaultAsync(uc => uc.Id == id);
+        if (existingComment != null)
+        {
+            _context.UserComments.Remove(existingComment);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
