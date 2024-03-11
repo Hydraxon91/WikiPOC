ea04b68e9a8717ede028459c28983c48a93f7f53
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-03-11 18:05:14 +0100

    Adds new method to wikiUserApi.js

--- wiki-frontend/src/Api/wikiUserApi.js
+++ wiki-frontend/src/Api/wikiUserApi.js
@@ -1,6 +1,6 @@
 const BASE_URL = 'http://localhost:5000';
 
-export const getUserProfileById = async (username, setUser) => {
+export const getUserProfileByUsername = async (username, setUser) => {
     const response = await fetch(`${BASE_URL}/api/UserProfile/GetByUserName/${username}`);
     if (!response.ok) {
       throw new Error(`Failed to get UserProfile for ${username}. Status: ${response.status}`);
@@ -27,3 +27,22 @@ export const getUserProfileById = async (username, setUser) => {
       const data = await response.json();
       return data;
   };
+
+export const postProfileEdit = async (profile, token) => {
+  const response = await fetch(`${BASE_URL}/api/UserProfile/UpdateProfile/${profile.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    },
+    body: JSON.stringify(profile),
+  });
+
+  if (!response.ok) {
+    // Handle the error, you can throw an exception or return an error object
+    throw new Error(`Failed to delete WikiPage. Status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  return data;
+};
