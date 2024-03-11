068fc5ba3c39bc29c32038259d74c16521340d1b
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-03-11 18:04:15 +0100

    Creates DisplayProfileImageElement.js

---
+++ wiki-frontend/src/Components/DisplayProfileImageElement.js
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react';
+import "../Styles/profilepage.css";
+
+function DisplayProfileImageElement({profilePicture}) {
+    useEffect(()=>{},[profilePicture])
+
+    const defaultImageUrl = 'https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg'; // Replace with your default image URL
+
+    return (
+        <div className='profile-picture-container'>
+            {profilePicture ? (<img src={profilePicture} alt="Uploaded" className='profile-picture'/>) : (<img src={defaultImageUrl} alt="Default" className='profile-picture'/>)}
+        </div>
+    );
+}
+
+export default DisplayProfileImageElement;
