ea43144a51ffabf95fe1f248354f795f673c8948
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-03-12 18:58:28 +0100

    Creates UserCommentRepositoryTests.Js with setup

---
+++ WikiPOC.Tests/UnitTests/RepositoryTests/UserCommentRepositoryTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using wiki_backend.DatabaseServices;
+using wiki_backend.DatabaseServices.Repositories;
+
+namespace UnitTests.RepositoryTests;
+[TestFixture]
+public class UserCommentRepositoryTests
+{
+    private UserCommentRepository _userCommentRepository;
+    private WikiDbContext _wikiDbContext;
+
+    [SetUp]
+    public void Setup()
+    {
+        // Generate a unique database name based on the test name
+        var databaseName = $"TestDatabase_{TestContext.CurrentContext.Test.Name}";
+        // Use an in-memory database for testing
+        var options = new DbContextOptionsBuilder<WikiDbContext>()
+            .UseInMemoryDatabase(databaseName: databaseName)
+            .Options;
+
+        // Use AddDbContext to configure the WikiDbContext
+        _wikiDbContext = new WikiDbContext(options, configuration: null); 
+        _wikiDbContext.Database.EnsureCreated(); // Ensure the in-memory database is created
+        _wikiDbContext.Database.EnsureDeleted();
+        _userCommentRepository = new UserCommentRepository(_wikiDbContext);
+    }
+    
+    [TearDown]
+    public void TearDown()
+    {
+        // Dispose the context to release the in-memory database
+        _wikiDbContext.Database.EnsureDeleted();
+        _wikiDbContext.Dispose();
+    }
+}
