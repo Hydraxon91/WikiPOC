e7cb1187ffc74eb5f15b7e34839f9f360a35b522
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-03-17 13:48:45 +0100

    DisplayProfileImageElement now fetches the pfp from the backend

--- wiki-frontend/src/Components/DisplayProfileImageElement.js
+++ wiki-frontend/src/Components/DisplayProfileImageElement.js
@@ -1,14 +1,32 @@
 import React, {useState, useEffect} from 'react';
+import { getProfilePicture } from '../Api/wikiUserApi';
 import "../Styles/profilepage.css";
 
 function DisplayProfileImageElement({profilePicture}) {
-    useEffect(()=>{},[profilePicture])
+    const defaultImageUrl = 'https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg'; 
+    const [imageSrc, setImageSrc] = useState(defaultImageUrl);
 
-    const defaultImageUrl = 'https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg'; // Replace with your default image URL
+    useEffect(()=>{
+        if (profilePicture) {
+            // Fetch profile picture when the component mounts or profilePicture prop changes
+            getProfilePicture(profilePicture)
+                .then(data => {
+                    const imageUrl = URL.createObjectURL(data);
+
+                    setImageSrc(imageUrl);
+                })
+                .catch(error => {
+                    console.error('Error fetching profile picture:', error);
+                    // Use default image URL in case of error
+                    setImageSrc('https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg');
+                });
+        }
+    },[profilePicture])
+
 
     return (
         <div className='profile-picture-container'>
-            {profilePicture ? (<img src={profilePicture} alt="Uploaded" className='profile-picture'/>) : (<img src={defaultImageUrl} alt="Default" className='profile-picture'/>)}
+            <img src={imageSrc} alt="Uploaded" className='profile-picture'/>
         </div>
     );
 }
