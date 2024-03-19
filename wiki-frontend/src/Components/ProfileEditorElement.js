9ea8fe229e5e0a5ec0b3ae4962e3648f3a20c93b
Hydraxon91 <benceszabowork@gmail.com>
2024-03-19 17:21:35 +0100

    Updates ProfileEditorElement

--- wiki-frontend/src/Components/ProfileEditorElement.js
+++ wiki-frontend/src/Components/ProfileEditorElement.js
@@ -7,14 +7,17 @@ import { postProfileEdit } from "../Api/wikiUserApi";
 const ProfileEditorElement = ({user, cookies}) => {
     const[profilePicture, setProfilePicture] = useState(null);
     const[displayName, setDisplayName] = useState(null);
+    const [profilePictureFile, setProfilePictureFile] = useState(null);
     const navigate = useNavigate();
 
     const handleDisplayNameChange = (event) => {
       setDisplayName(event.target.value);
     };
 
-    const handlePfpLinkChange = (event) => {
-      setProfilePicture(event.target.value);
+    const handleProfilePictureFileChange = (event) => {
+      const file = event.target.files[0];
+      setProfilePictureFile(file);
+      setProfilePicture(URL.createObjectURL(file));
     };
 
     const handleSubmit = async (e) =>{
@@ -22,11 +25,10 @@ const ProfileEditorElement = ({user, cookies}) => {
       const newProfile = {
         ...user,
         displayName: displayName,
-        profilePicture: profilePicture
       }
 
       try {
-        await postProfileEdit(newProfile, cookies);
+        await postProfileEdit(newProfile, profilePictureFile, cookies);
         alert('Successfully submitted profile update');
         navigate(`/profile/${newProfile.userName}`)
     } catch (error) {
@@ -39,9 +41,6 @@ const ProfileEditorElement = ({user, cookies}) => {
         // console.log(user);
         if (user?.displayName) {
           setProfilePicture(user.profilePicture);
-          if (user.profilePicture) {
-            setProfilePicture(user.profilePicture);
-          }
           setDisplayName(user.displayName);
         }
       },[user])
@@ -53,7 +52,12 @@ const ProfileEditorElement = ({user, cookies}) => {
              </div>
              <div className="user-info">
                 <p>Profile Picture: </p>
-                <input className="edit-displayname" type="text" value={profilePicture} onChange={handlePfpLinkChange}/>
+                <input
+                  className="edit-displayname"
+                  type="file"
+                  accept="image/*"
+                  onChange={handleProfilePictureFileChange}
+                />
              </div>
              
              <div className="user-info"><p>Username: {user?.userName}</p></div>
