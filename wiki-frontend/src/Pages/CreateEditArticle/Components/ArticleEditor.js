8348bd81b5256617e24f8fa2cb9ddcddd3bd10a2
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-04-14 12:43:13 +0200

    WIP: Adds the ability to add Images to the right side of the article in the editor

--- wiki-frontend/src/Pages/CreateEditArticle/Components/ArticleEditor.js
+++ wiki-frontend/src/Pages/CreateEditArticle/Components/ArticleEditor.js
@@ -1,9 +1,97 @@
-import React, { useRef } from 'react';
-import ReactQuill from 'react-quill';
+import React, { useRef, useEffect } from 'react';
+import ReactQuill, { Quill } from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+
+// Define a custom blot for the custom HTML structure
+const CustomHTMLBlot = Quill.import('blots/block/embed')
+class CustomQuillHTML extends CustomHTMLBlot{
+    static create(value) {
+        const node = super.create(value);
+        // Set the innerHTML of the node based on the value
+        node.innerHTML = value.content;
+        // Set other attributes or styles if needed
+        return node;
+    }
+    
+    static value(node) {
+        return { content: node.innerHTML };
+      }
+    
+      static formats(node) {
+        // Return formats for the node, if necessary
+        return {};
+      }
+    
+      static sanitize(value) {
+        // Sanitize the value, if necessary
+        return value;
+      }
+}
+CustomQuillHTML.blotName = 'custom-html';
+CustomQuillHTML.tagName = 'div';
+Quill.register(CustomQuillHTML);
+
 
 const ArticleEditor = ({ title, siteSub, roleNote, content, handleFieldChange, handleContentChange, handleSave }) => {
   const quillRef = useRef(null); // Define quillRef
+
+  useEffect(() => {
+    if (quillRef.current) {
+      // Initialize Quill editor
+      const editor = quillRef.current.getEditor();
+      if (editor) {
+        editor.enable(true);
+        editor.getModule('toolbar').addHandler('custom-html', insertCustomHTML);
+      }
+    }
+  }, []);
+
+  const customModules = {
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike'],
+      [{ 'header': [2, 3, false] }],
+      ['link'],
+      [{ 'image': {} }, { 'custom-html': {} }]
+    ],
+  };
+
+  // Handler for inserting the custom HTML structure
+//   const insertCustomHTML = () => {
+//     const editor = quillRef.current?.getEditor();
+//     if (editor) {
+//         // Get the current selection
+//         const range = editor.getSelection();
+//         if (range) {
+//             // Disable the editor's autocompletion of newline after insertion
+//             const htmlContent = '<div class="articleRight"><div class="articleRightInner"><img class="paragraphImage" src="https://image.api.playstation.com/vulcan/ap/rnd/202309/0718/2c253de3117182b4a09d02ad16ebc51a25d4ea9208a5d057.jpg" alt="logo"></div><div class="wikipage-content-container">Helldivers never die!</div></div>';
+//             editor.insertText(range.index, htmlContent, 'user');
+
+//             // Manually set the selection after the inserted content
+//             editor.setSelection(range.index + htmlContent.length);
+//         } else {
+//             console.error('Could not get current selection.');
+//         }
+//     }
+// };
+
+const insertCustomHTML = () => {
+    const editor = quillRef.current?.getEditor();
+    if (editor) {
+      const range = editor.getSelection();
+      if (range) {
+        const htmlContent = '<div class="articleRight"><div class="articleRightInner"><img class="paragraphImage" src="https://image.api.playstation.com/vulcan/ap/rnd/202309/0718/2c253de3117182b4a09d02ad16ebc51a25d4ea9208a5d057.jpg" alt="logo"></div><div class="wikipage-content-container">Helldivers never die!</div></div>';
+        editor.insertEmbed(
+          range.index, 
+          'custom-html', // Custom format name
+          { content: htmlContent }
+        );
+      } else {
+        console.error('Could not get current selection.');
+      }
+    }
+  };
+
+  
 
   const handleImageUpload = async (file) => {
     // Upload image file to server
@@ -56,29 +144,17 @@ const ArticleEditor = ({ title, siteSub, roleNote, content, handleFieldChange, h
           onChange={(e) => handleFieldChange('roleNote', e.target.value)} 
         />
       </div>
-      {/* <ReactQuill 
-        theme="snow" 
-        value={content} 
-        onChange={handleContentChange}
-        modules={{
-          toolbar: {
-            container: [
-              [{ 'header': [1, 2, false] }],
-              ['bold', 'italic', 'underline'],
-              ['image'], // Include image button in toolbar
-            ],
-            handlers: {
-              'image': handleImageUpload, // Custom handler for image button
-            },
-          },
-        }}
-        ref={quillRef} // Assign the ref to the Quill editor
-      /> */}
-      <ReactQuill 
-        theme="snow" 
-        value={content} 
-        onChange={handleContentChange} 
-      />
+      <div>
+        <ReactQuill 
+            theme="snow" 
+            value={content} 
+            onChange={handleContentChange}
+            modules={customModules}
+            ref={quillRef}
+        />
+        <button onClick={insertCustomHTML}>Insert Custom HTML</button>
+      </div>
+      
       <button onClick={() => handleSave(content)}>Save</button>
     </div>
   );
