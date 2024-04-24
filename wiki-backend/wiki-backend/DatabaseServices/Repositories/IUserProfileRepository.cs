532b902ff4050a6ab170ecb572435d9fe3195140
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-04-24 17:13:40 +0200

    Updates UserProfileRepository

--- wiki-backend/wiki-backend/DatabaseServices/Repositories/IUserProfileRepository.cs
+++ wiki-backend/wiki-backend/DatabaseServices/Repositories/IUserProfileRepository.cs
@@ -4,11 +4,11 @@ namespace wiki_backend.DatabaseServices.Repositories;
 
 public interface IUserProfileRepository
 {
-    Task<UserProfile?> GetByIdAsync(int id);
+    Task<UserProfile?> GetByIdAsync(Guid id);
     Task<UserProfile?> GetByUsernameAsync(string username);
     Task<UserProfile?> GetByUserIdAsync(string id);
     //Not needed for now
     // Task AddAsync(UserProfile wikiPage);
-    Task UpdateAsync(int existingId , UserProfile updatedProfile, IFormFile? profilePictureFile);
-    Task DeleteAsync(int id);
+    Task UpdateAsync(Guid existingId , UserProfile updatedProfile, IFormFile? profilePictureFile);
+    Task DeleteAsync(Guid id);
 }
