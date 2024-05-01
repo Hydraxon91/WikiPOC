4d3aff6e14126065309968cd1bde7a2c94dc88d6
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-05-01 16:36:51 +0200

    If there are no pages in a looked up category it will say so

--- wiki-frontend/src/Pages/Categories/CategoryPageComponent.js
+++ wiki-frontend/src/Pages/Categories/CategoryPageComponent.js
@@ -4,6 +4,7 @@ import { Link, useParams } from 'react-router-dom';
 const CategoryPageComponent = ({ pages }) => {
   const { category } = useParams(); // Extract category from URL
   const [pagesByCategory, setPagesByCategory] = useState({});
+  const [pagesInCurrentCategory, setPagesInCurrentCategory] = useState([])
 
   useEffect(() => {
     // Organize pages by category
@@ -17,23 +18,26 @@ const CategoryPageComponent = ({ pages }) => {
     setPagesByCategory(pagesByCategory);
   }, [pages]);
 
+  useEffect(()=>{
+    setPagesInCurrentCategory(pagesByCategory[category] || [])
+  },[pagesByCategory])
+
   return (
     <div className='home-component'>
       <h2>Category: {category}</h2>
-      {Object.entries(pagesByCategory).map(([cat, pages]) => (
-        cat === category && // Only render pages for the current category
-        <div key={cat}>
-          <ul>
-            {pages.map((page, index) => (
-              <li key={index}>
-                <Link to={`/page/${encodeURIComponent(page.title)}`}>
-                  {page.title}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
+      {pagesInCurrentCategory.length > 0 ? (
+        <ul>
+          {pagesInCurrentCategory.map((page, index) => (
+            <li key={index}>
+              <Link to={`/page/${encodeURIComponent(page.title)}`}>
+                {page.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>There are no pages in this category yet.</p>
+      )}
     </div>
   );
 };
