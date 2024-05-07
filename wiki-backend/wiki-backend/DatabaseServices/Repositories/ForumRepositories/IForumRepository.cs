11c174a6e72342b9b16de98d1ba4a0d202740a0f
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-05-07 18:08:39 +0200

    Creates IForumRepository.cs

---
+++ wiki-backend/wiki-backend/DatabaseServices/Repositories/ForumRepositories/IForumRepository.cs
@@ -0,0 +1,12 @@
+﻿using wiki_backend.Models.ForumModels;
+
+namespace wiki_backend.DatabaseServices.Repositories.ForumRepositories;
+
+public interface IForumRepository
+{
+    Task<ForumPost> GetForumPostByIdAsync(Guid id);
+    Task<IEnumerable<ForumPost>> GetAllForumPostsAsync();
+    Task AddForumPostAsync(ForumPost post);
+    Task UpdateForumPostAsync(ForumPost post);
+    Task DeleteForumPostAsync(Guid id);
+}
