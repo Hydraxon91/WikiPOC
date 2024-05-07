4f24225a76e055da6c5c62aeef05a37503703726
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-05-07 18:06:55 +0200

    Creates ForumPost Model

---
+++ wiki-backend/wiki-backend/Models/ForumModels/ForumPost.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace wiki_backend.Models.ForumModels;
+
+public class ForumPost
+{
+    public Guid Id { get; set; }
+    [Required]
+    public string PostTitle { get; set; }
+    [Required]
+    public string Content { get; set; }
+    public ICollection<UserComment> Comments { get; set; } = new List<UserComment>();
+    public DateTime? PostDate { get; set; }
+
+    // Reference to the topic/category
+    public ForumTopic ForumTopic  { get; set; }
+    public Guid ForumTopicId { get; set; }
+}
