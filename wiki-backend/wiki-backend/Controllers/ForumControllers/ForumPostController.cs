de8f88dd42721f343dbb0abc2b2c6993bc1c962e
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-05-08 18:18:39 +0200

    Creates ForumPostController.cs

---
+++ wiki-backend/wiki-backend/Controllers/ForumControllers/ForumPostController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using wiki_backend.DatabaseServices.Repositories.ForumRepositories;
+
+namespace wiki_backend.Controllers.ForumControllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ForumPostController : ControllerBase
+{
+    private readonly IForumPostRepository _forumPostRepository;
+
+    public ForumPostController(IForumPostRepository forumPostRepository)
+    {
+        _forumPostRepository = forumPostRepository;
+    }
+    
+    
+}
