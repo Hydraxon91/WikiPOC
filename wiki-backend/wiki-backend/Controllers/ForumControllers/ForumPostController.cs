98daa18b35219e0e0aa1cf9fdb51905757558808
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-05-08 18:20:28 +0200

    Adds GetForumPostsTitles method to ForumPostController

--- wiki-backend/wiki-backend/Controllers/ForumControllers/ForumPostController.cs
+++ wiki-backend/wiki-backend/Controllers/ForumControllers/ForumPostController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using wiki_backend.DatabaseServices.Repositories.ForumRepositories;
+using wiki_backend.Models.ForumModels;
 
 namespace wiki_backend.Controllers.ForumControllers;
 
@@ -14,5 +15,10 @@ public class ForumPostController : ControllerBase
         _forumPostRepository = forumPostRepository;
     }
     
-    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<string>>> GetForumPostsTitles()
+    {
+        var forumPosts = await _forumPostRepository.GetAllForumPostTitlesAsync();
+        return Ok(forumPosts);
+    }
 }
