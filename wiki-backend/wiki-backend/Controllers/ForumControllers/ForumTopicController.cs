8c76fc8824d1becc742cf5ba6316a6d3c56ea4e9
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-05-08 18:50:28 +0200

    Creates ForumTopicController.cs

---
+++ wiki-backend/wiki-backend/Controllers/ForumControllers/ForumTopicController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using wiki_backend.DatabaseServices.Repositories.ForumRepositories;
+
+namespace wiki_backend.Controllers.ForumControllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ForumTopicController : ControllerBase
+{
+    private readonly IForumTopicRepository _forumTopicRepository;
+
+    public ForumTopicController(IForumTopicRepository forumTopicRepository)
+    {
+        _forumTopicRepository = forumTopicRepository;
+    }
+}
