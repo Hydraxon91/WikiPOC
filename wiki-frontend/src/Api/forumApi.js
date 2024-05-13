10932938dc89a6b0a36c7728510c0c9e431b96a8
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-05-13 18:22:38 +0200

    Adds basic forumtopic methods for forumtopics

--- wiki-frontend/src/Api/forumApi.js
+++ wiki-frontend/src/Api/forumApi.js
@@ -1,1 +1,61 @@
 const BASE_URL = process.env.REACT_APP_API_URL;
+
+export const getForumTopics = async () => {
+    const response = await fetch(`${BASE_URL}/api/ForumTopic`);
+    if (!response.ok) {
+      throw new Error(`Failed to get WikiPages. Status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  };
+
+
+  export const createForumTopic = async (forumTopic, token) => {
+    const response = await fetch(`${BASE_URL}/api/ForumTopic`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      },
+      body: JSON.stringify(forumTopic),
+    });
+  
+    if (!response.ok) {
+      throw new Error(`Failed to create ForumTopic. Status: ${response.status}`);
+    }
+  
+    return await response.json();
+  };
+
+  export const updateForumTopic = async (forumTopic, token) => {
+    const response = await fetch(`${BASE_URL}/api/ForumTopic/${forumTopic.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      },
+      body: JSON.stringify(forumTopic),
+    });
+  
+    if (!response.ok) {
+      throw new Error(`Failed to update ForumTopic. Status: ${response.status}`);
+    }
+  
+    return await response.json();
+  };
+
+  export const deleteForumTopic = async (forumTopic, token) => {
+   
+    const response = await fetch(`${BASE_URL}/api/ForumTopic/${forumTopic.id}`, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${token}`,
+      },
+    });
+  
+    if (!response.ok) {
+      throw new Error(`Failed to delete ForumTopic. Status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  };
