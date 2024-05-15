61032b0f335915b238b60ee1ffa4f6f21474bd0c
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-05-15 18:05:44 +0200

    Improves forumpost.js design

--- wiki-frontend/src/Pages/ForumPages/ForumPost.js
+++ wiki-frontend/src/Pages/ForumPages/ForumPost.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { getForumPostBySlug } from '../../Api/forumApi';
 import ForumCommentComponent from './Components/ForumCommentComponent';
+import DisplayProfileImageElement from '../ProfilePage/Components/DisplayProfileImageElement';
+import "./Styles/forumpost.css"
 
 const ForumPost = ({cookies}) => {
     const [post, setPost] = useState(null);
@@ -11,6 +14,7 @@ const ForumPost = ({cookies}) => {
         const fetchForumPost = async () => {
             try {
                 const fetchedPost = await getForumPostBySlug(slug);
+                console.log(fetchedPost);
                 setPost(fetchedPost);
             } catch (error) {
                 console.error("Error fetching post:", error);
@@ -25,12 +29,30 @@ const ForumPost = ({cookies}) => {
     }
 
     return (
-        <div>
-            <h1>Post Subject: {post.postTitle} Posted: {new Date(post.postDate).toLocaleString()}</h1>
-            <p>{post.content}</p>
-            <p>Posted by: {post.userName}</p>
+        <>
+        <div className="forumpost-grid">
+            <div className="fp-grid-header">
+                <div className="fp-header-cell">Author</div>
+                <div className="fp-header-cell">Message</div>
+            </div>
+            <div className="fp-grid-row">
+                <div className="fp-grid-cell"><Link to={`/profile/${post.userName}`}>{post.user.displayName}</Link></div>
+                <div className="fp-grid-cell">
+                    <div className='fp-grid-cell-left'>Post Subject: {post.postTitle}</div>
+                    <div className='fp-grid-cell-right'>Posted: {new Date(post.postDate).toLocaleString()}</div>
+                </div>
+            </div>
+            <div className="fp-grid-row">
+                <div className="fp-grid-cell">
+                    <DisplayProfileImageElement profilePicture={post.user.profilePicture}/>
+                </div>
+                <div className="fp-grid-cell">{post.content}</div>
+            </div>
+            
+            
             <ForumCommentComponent post={post} cookies={cookies}/>
         </div>
+        </>
     );
 };
 
