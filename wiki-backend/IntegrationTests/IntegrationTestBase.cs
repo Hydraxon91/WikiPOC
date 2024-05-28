6c7497b69a39bd482bbc2f2504889d1e7b5c4922
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-05-28 11:07:41 +0200

    Creates IntegrationTestBase.cs

---
+++ wiki-backend/IntegrationTests/IntegrationTestBase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using wiki_backend.DatabaseServices;
+
+namespace IntegrationTests;
+
+public class IntegrationTestBase : IDisposable
+{
+    protected readonly ServiceProvider ServiceProvider;
+    protected readonly WikiDbContext DbContext;
+    
+    public IntegrationTestBase()
+    {
+        var services = new ServiceCollection();
+        services.AddDbContext<WikiDbContext>(options =>
+            options.UseSqlServer("YourTestDatabaseConnectionString"));
+        
+        ServiceProvider = services.BuildServiceProvider();
+
+        DbContext = ServiceProvider.GetRequiredService<WikiDbContext>();
+        DbContext.Database.EnsureCreated();
+    }
+
+    public void Dispose()
+    {
+        DbContext.Database.EnsureDeleted();
+        DbContext.Dispose();
+    }
+}
